d670c69cbd8ece45e89b99c3b2e96580c30577f4
Marko Avlijaš <marko.avlijas@gmail.com>
2020-09-06 19:24:21 +0200

    Detect operating system

--- lib/system_helper.rb
+++ lib/system_helper.rb
@@ -1,5 +1,9 @@
+require "open3"
+
 class SystemHelper
   class << self
+
+    # downloads file using download tool (curl or wget)
     def download_file(url:, to:, download_tool:)
       case download_tool.to_s
       when 'curl'
@@ -8,6 +12,52 @@ class SystemHelper
         system %{wget -O "#{to}" "#{url}"}
       else
         raise "Invalid value for Settings.instance.download_tool: #{download_tool.inspect}"
+      end
+    end
+
+    # checks if app is installed
+    def installed?(app)
+      system "which #{app} >/dev/null"
+    end
+
+    # Passes *commands to Open3.capture3 which passes it to system.
+    # Returns stdout.
+    #
+    # Raises StandardError if system returns non 0 exit code (failure).
+    # Error message contains exit code and stderr output.
+    def capture_stdout_and_raise_on_error(*commands)
+      stdout, stderr, status = Open3.capture3(*commands)
+
+      unless status.success?
+        raise "#{commands.join(" ")} failed with exit code #{status.exitstatus}.\n#{stderr}"
+      end
+
+      stdout
+    end
+
+    # detects operating system
+    # returns one of :linux, :android, :darwin, :windows, ...
+    #
+    # based on https://github.com/rossmacarthur/install/blob/master/crate.sh
+    def detect_os
+      kernel_name = capture_stdout_and_raise_on_error("uname", "-s").strip
+
+      case kernel_name
+      when "Linux"
+        os = capture_stdout_and_raise_on_error("uname", "-o")
+        (os =~ /Android/) ? :android : :linux
+      when "Darwin"
+        :darwin
+      when /MINGW/, /MSYS/, /CYGWIN/
+        :windows
+      when "FreeBSD"
+        :freebsd
+      when "NetBSD"
+        :netbsd
+      when "DragonFly"
+        :dragonfly
+      else
+        :unknown_os
       end
     end
   end
--- spec/lib/system_helper_spec.rb
+++ spec/lib/system_helper_spec.rb
@@ -26,4 +26,110 @@ describe SystemHelper do
       SystemHelper.download_file(url: url, to: to, download_tool: 'curl')
     end
   end
+
+  describe ".installed?" do
+    it "detects when app is not installed" do
+      allow(SystemHelper).to receive("system").with("which wget >/dev/null").and_return(false)
+
+      expect(SystemHelper.installed?("wget")).to be false
+    end
+
+    it "detects when app is installed" do
+      allow(SystemHelper).to receive("system").with("which wget >/dev/null").and_return(true)
+
+      expect(SystemHelper.installed?("wget")).to be true
+    end
+  end
+
+  describe ".capture_stdout_and_raise_on_error" do
+    context "command returns 0 (success)" do
+      it "returns captured stdout" do
+        output = SystemHelper.capture_stdout_and_raise_on_error("which", "bash")
+        expect(output).to match /bash\n$/
+      end
+    end
+
+    context "command returns non 0 (error)" do
+      it "raises StandardError with message containing exit code and stderr" do
+        error_code_message = /uname --racket failed with exit code 1./
+        stderr_message = /uname: /
+
+        expect do
+          SystemHelper.capture_stdout_and_raise_on_error("uname", "--racket")
+        end.to raise_error(StandardError, error_code_message)
+
+        expect do
+          SystemHelper.capture_stdout_and_raise_on_error("uname", "--racket")
+        end.to raise_error(StandardError, stderr_message)
+      end
+    end
+  end
+
+  describe ".detect_os" do
+    let (:success_status) do
+      o = Object.new
+      class << o
+        def success?
+          true
+        end
+      end
+
+      o
+    end
+
+    it "detects Linux" do
+      allow(Open3).to receive(:capture3).with("uname", "-s").and_return(["Linux\n", "", success_status])
+      allow(Open3).to receive(:capture3).with("uname", "-o").and_return(["GNU/Linux\n", "", success_status])
+
+      expect(SystemHelper.detect_os).to eq(:linux)
+    end
+
+    it "detects Android" do
+      allow(Open3).to receive(:capture3).with("uname", "-s").and_return(["Linux\n", "", success_status])
+      allow(Open3).to receive(:capture3).with("uname", "-o").and_return(["Android\n", "", success_status])
+
+      expect(SystemHelper.detect_os).to eq(:android)
+    end
+
+    it "detects Darwin" do
+      allow(Open3).to receive(:capture3).with("uname", "-s").and_return(["Darwin\n", "", success_status])
+
+      expect(SystemHelper.detect_os).to eq(:darwin)
+    end
+
+    it "detects FreeBSD" do
+      allow(Open3).to receive(:capture3).with("uname", "-s").and_return(["FreeBSD\n", "", success_status])
+
+      expect(SystemHelper.detect_os).to eq(:freebsd)
+    end
+
+    it "detects NetBSD" do
+      allow(Open3).to receive(:capture3).with("uname", "-s").and_return(["NetBSD\n", "", success_status])
+
+      expect(SystemHelper.detect_os).to eq(:netbsd)
+    end
+
+    it "detects DragonFly" do
+      allow(Open3).to receive(:capture3).with("uname", "-s").and_return(["DragonFly\n", "", success_status])
+
+      expect(SystemHelper.detect_os).to eq(:dragonfly)
+    end
+
+    it "detects Windows" do
+      allow(Open3).to receive(:capture3).with("uname", "-s").and_return(["MINGWblah\n", "", success_status])
+      expect(SystemHelper.detect_os).to eq(:windows)
+
+      allow(Open3).to receive(:capture3).with("uname", "-s").and_return(["MSYSblah\n", "", success_status])
+      expect(SystemHelper.detect_os).to eq(:windows)
+
+      allow(Open3).to receive(:capture3).with("uname", "-s").and_return(["CYGWINblah\n", "", success_status])
+      expect(SystemHelper.detect_os).to eq(:windows)
+    end
+
+    it "returns :unknown_os if can't recognize os" do
+      allow(Open3).to receive(:capture3).with("uname", "-s").and_return(["Hey Ho\n", "", success_status])
+
+      expect(SystemHelper.detect_os).to eq(:unknown_os)
+    end
+  end
 end
